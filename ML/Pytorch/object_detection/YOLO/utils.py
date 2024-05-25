00c994103361778ad68d308d7b2539d3c54f8a58
kch1015 <songwoo1015@gmail.com>
2024-05-25 22:48:09 +0900

    feat: mean average precision 함수 구현

--- ML/Pytorch/object_detection/YOLO/utils.py
+++ ML/Pytorch/object_detection/YOLO/utils.py
@@ -230,6 +230,106 @@ def mean_average_precision(
     return sum(average_precisions) / len(average_precisions)
 
 
+def map(
+        pred_boxes: list[list[int | float]],
+        true_boxes: list[list[int | float]],
+        iou_threshold=0.5,
+        box_format="midpoint",
+        num_classes=20,
+        epsilon=1e-6
+) -> float:
+    """
+    mAP 값을 계산하여 반환한다.
+
+    Parameters:
+        pred_boxes (list): [train_idx, class_prediction, prob_score, x1, y1, x2, y2]로 이루어진 list
+        true_boxes (list): [train_idx, class_idx, prob_score(있으면 1, 없으면 0), x1, y1, x2, y2]로 이루어진 list
+        iou_threshold (float): TP, FP를 결정하는 기준값
+        box_format (str): bounding box 형식, "midpoint" 또는 "corners"
+        num_classes (int): 클래스 수
+
+    Returns:
+        float: iou_threshold 값을 기준으로 모든 클래스에 대한 평균 mAP를 반환한다.
+    """
+    average_precisions = []
+
+    # class 별로 분류
+    detections = [[] for _ in range(num_classes)]
+    for detection in pred_boxes:
+        detections[detection[1]].append(detection)
+
+    ground_truths = [[] for _ in range(num_classes)]
+    for true_box in true_boxes:
+        ground_truths[true_box[1]].append(true_box)
+
+    for c in range(num_classes):
+        if not ground_truths[c]:
+            continue
+
+        # train_idx 별로 몇 개인지 셈
+        amount_bboxes = Counter([gt[0] for gt in ground_truths[c]])
+
+        # ground_truth 판정 여부 저장
+        is_evaluated = defaultdict(bool)
+        for key, val in amount_bboxes.items():
+            is_evaluated[key] = [False] * val
+
+        # confidence 기준 내림차순으로 정렬
+        class_detections = sorted(detections[c], key=lambda x: x[2], reverse=True)
+
+        # TP, FP 여부 저장
+        TP = torch.zeros((len(class_detections)))
+        FP = torch.zeros((len(class_detections)))
+
+        data_ground_truths = defaultdict(list)
+        for bbox in ground_truths[c]:
+            data_ground_truths[bbox[0]].append(bbox)
+
+        for detection_idx, detection in enumerate(class_detections):
+            data_ground_truth = data_ground_truths[detection[0]]
+
+            best_iou = 0
+            best_gt_idx = 0
+
+            for data_idx, gt in enumerate(data_ground_truth):
+                iou = intersection_over_union(
+                    torch.tensor(detection[3:]),
+                    torch.tensor(gt[3:]),
+                    box_format=box_format,
+                )
+
+                if best_iou < iou:
+                    best_iou = iou
+                    best_gt_idx = data_idx
+
+            # iou_threshold 초과이고 이미 판정되지 않았으면(정렬되어 있어 이전의 결과가 최선임을 보장) TP, 아니면 FP
+            if best_iou > iou_threshold and is_evaluated[detection[0]][best_gt_idx] == 0:
+                TP[detection_idx] = 1
+                is_evaluated[detection[0]][best_gt_idx] = 1
+            else:
+                FP[detection_idx] = 1
+
+        # TP, FP의 누적합
+        TP = torch.cumsum(TP, dim=0)
+        FP = torch.cumsum(FP, dim=0)
+
+        # recall = TP / (TP + FN) = TP / all ground truths
+        # precision = TP / (TP + FP) = TP / all detections
+        num_ground_truths = len(ground_truths[c])
+        recalls = TP / (num_ground_truths + epsilon)
+        precisions = TP / (TP + FP + epsilon)
+
+        # (0, 1) 추가
+        recalls = torch.cat((torch.tensor([0]), recalls))
+        precisions = torch.cat((torch.tensor([1]), precisions))
+
+        # PR 곡선의 적분값 = AP
+        average_precisions.append(torch.trapz(precisions, recalls))
+
+    # AP의 평균 = mAP
+    return sum(average_precisions) / len(average_precisions)
+
+
 def plot_image(image, boxes):
     """Plots predicted bounding boxes on the image"""
     im = np.array(image)
@@ -357,14 +457,15 @@ def convert_cellboxes(predictions, S=7, C=11):
 
 def cellboxes_to_boxes(out, S=7, C=11):
     converted_pred = convert_cellboxes(out, S, C).reshape(out.shape[0], S * S, -1)
-    converted_pred[..., 0] = converted_pred[..., 0].long()
     all_bboxes = []
 
     for ex_idx in range(out.shape[0]):
         bboxes = []
 
         for bbox_idx in range(S * S):
-            bboxes.append([x.item() for x in converted_pred[ex_idx, bbox_idx, :]])
+            items = [x.item() for x in converted_pred[ex_idx, bbox_idx, :]]
+            items[0] = int(items[0])
+            bboxes.append(items)
         all_bboxes.append(bboxes)
 
     return all_bboxes
